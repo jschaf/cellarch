f75b4976ffa396062cd260000aab2c086d8c9264
Joe Schafer <joe@jschaf.com>
2019-05-17 14:18:24 -0700

    Tidy up

--- montecarlo.py
+++ montecarlo.py
@@ -1,14 +1,38 @@
 """
-TODO: model data placement as a distribution.  Currently uniform.
-TODO: model mean time to repair as a distribution
-TODO: model mean time to failure as a distribution
+A monte-carlo simulation to model failures in a distributed storage system.
 
-𝑛 - number of machines. A machine exists in exactly 1 cell.
-𝑐 - the number of cells. A cell is a group of machines. Assume each cell contains the same number of machines.
-𝑓 - mean time to failure of an individual machine.
-𝑟 - the number of replicas for each piece of data. If data is replicated twice, it exists on 2 separate machines in the same cell.
-𝑑 - the number of pieces of data in the system. Data is uniformly distributed across cells and each cell uniformly distributes data across machines.
-𝑡 - the recovery time for a machine after it fails.
+Math StackExchange question: https://math.stackexchange.com/q/3217875/31502
+
+In broad strokes, the simulation proceeds as follows:
+
+-   Partition NUM_MACHINES into NUM_PARTITIONS separate groups.  This simulation
+    uses partition to refer to separate groups instead of `cell` used by the math
+    StackExchange question.
+
+-   Uniformly distribute NUM_DATA pieces of data to all subsets of machines such
+    that:
+
+    1.  Every subset of machines reside in the same partition.
+    2.  Every subset has exactly a size of NUM_REPLICAS.
+
+-   Generate machine failure start times pulling samples from an exponential
+    distribution.
+
+-   Get the cumulative sum of the failure times to generate subsequent failure
+    times for a machine.  Meaning, turn [1, 3, 2, 7] into [1, 4, 6, 13].
+
+-   Create an outage for a machine by adding the time to repair to the failure
+    start time.  The time to repair is drawn from a normal distribution.
+
+-   Find all outages where N machines are down at the same time.  This is an
+    outage clique.  When N == NUM_REPLICAS, this means we might have an outage
+    for some subset of data.
+
+-   Find all outage cliques where each machine in the clique hosts the same
+    piece of data.  The found cliques mean some data is completely unavailable.
+
+-   Display the outage cliques start time, duration, and machines in the clique.
+
 """
 # %% Imports
 import dataclasses
@@ -16,34 +40,27 @@ from dataclasses import dataclass
 from typing import Dict, List, FrozenSet, NewType, Callable
 import itertools
 
-import numpy as np
-import seaborn as sns
-import matplotlib as plt
 import datetime
+import numpy as np
 import humanize
 
 # %%
 
-style = sns.set_style('whitegrid')
-# Make plots work in IntelliJ
-plt.interactive(False)
-
-# %%
-
-# A one-dimensional, random distribution that takes a int size
-# and returns samples from the distribution.  For example, here's
-# a RandomDist from a uniform distribution with a mean of 5 and
-# stddev of 2:
+# A random distribution that takes a int size and returns samples
+# from the distribution.  For example, here's a random dist from
+# a uniform distribution with a mean of 5 and stddev of 2:
 #
 #     lambda size: np.random.normal(5, 2, size)
-RandomDist1dFn = NewType('RandomDist', Callable[[int], np.ndarray])
+RandomDistFn = NewType('RandomDist', Callable[[int], np.ndarray])
 
 
 def hours(n: int) -> datetime.timedelta:
+    """Creates a timedelta of exactly n hours."""
     return datetime.timedelta(hours=n)
 
 
 def days(n: int):
+    """Creates a timedelta of exactly n days."""
     return datetime.timedelta(days=n)
 
 
@@ -51,27 +68,70 @@ def total_hours(delta: datetime.timedelta) -> int:
     """Converts a timedelta into the number of hours.
 
     Timedelta normalizes the delta into days, seconds and microseconds so
-    'delta.hours' only returns the hours passed into the constructor."""
+    'delta.hours' only returns the hours passed into the constructor.  We
+    typically want the total elapsed time."""
     return int(delta.total_seconds()) // 3600
+
+
+def total_days(delta: datetime.timedelta) -> int:
+    """Converts a timedelta into the total number of days.
+
+    See total_hours."""
+    return total_hours(delta) // 24
+
+
+def total_months(delta: datetime.timedelta) -> int:
+    """Converts a timedelta into the total number of months.
+
+    See total_hours."""
+    return total_days(delta) // 30
 
 
 def time_to_failure_exp_dist(
         num_machines: int,
         mttf: datetime.timedelta,
         sim_duration: datetime.timedelta) -> np.ndarray:
-    """Generates a two-dimensional array of increasing failure times of
-    integer hours from an exponential distribution.
+    """Generates a two-dimensional array of monotonically increasing failure
+    times of integer hours from an exponential distribution.
 
     The returned ndarray has `num_machines` rows.  Each row represents the
-    start of failures for a single machine.  Each row has the same number
+    start times of failures for a single machine.  Each row has the same number
     of columns.  The last value of each column is guaranteed to exceed the
     simulation duration.
+
+    For example, for 2 machines this method might produce:
+
+        [[1, 5, 22], [7, 9, 15]]
+
+    Meaning machine 0 has a failures that start at hours 1, 5, and 22.
+    Machine 1 has failures that start at hours 7, 9, and 15.
+
+    The exponential distribution is a good candidate for the reasons described
+    in 'Availability in Globally Distributed Storage Systems' [1].
+
+    > The exponential distribution is a reasonable approximation for the
+    > following reasons. First, the Weibull distribution is a generalization
+    > of the exponential distribution that allows the rate parameter to
+    > increase over time to reflect the aging of disks. In a large
+    > population of disks, the mixture of disks of different ages tends to
+    > be stable, and so the average failure rate in a cell tends to be
+    > constant. When the failure rate is stable, the Weibull distribution
+    > provides the same quality of fit as the exponential. Second, disk
+    > failures make up only a small subset of failures that we examined, and
+    > model results indicate that overall availability is not particularly
+    > sensitive to them. Finally, other authors ([24]) have concluded that
+    > correlation and non-homogeneity of the recovery rate and the mean time
+    > to a failure event have a much smaller impact on system-wide
+    > availability than the size of the event.
+
+    [1]: http://static.googleusercontent.com/media/research.google.com/en//pubs/archive/36737.pdf
     """
     mttf_hours = total_hours(mttf)
     sim_hours = total_hours(sim_duration)
     # Use a generous fudge factor so we only have to drop columns, not add
-    # new columns to ensure the last column > min_duration.
-    fudge_factor = 5
+    # new columns to ensure the min(last_column) > sim_duration.  Surely, there's
+    # a cleaner way to do this.
+    fudge_factor = 10
     num_cols = (sim_hours // mttf_hours) * fudge_factor
 
     # Add 1 to avoid having duplicate failure times if we get 0 from the
@@ -80,12 +140,12 @@ def time_to_failure_exp_dist(
     reshaped = np.reshape(all_failures, (num_machines, num_cols))
     summed = np.cumsum(reshaped, 1)
 
-    # Drop excess columns
+    # Drop columns after the first column where min(col) > sim_duration.
     col_mins = np.min(summed, axis=0)
     cols_exceeding_sim_duration = np.nonzero(col_mins > sim_hours)[0]
     assert cols_exceeding_sim_duration.size > 0, (
         'Not enough samples to exceed sim duration for all columns')
-    # Add 1 because we want to keep the last column.
+    # Add 1 because we want to keep the column we found.
     cols_to_keep = cols_exceeding_sim_duration[0] + 1
     return np.delete(summed, np.s_[cols_to_keep:], axis=1)
 
@@ -93,10 +153,10 @@ def time_to_failure_exp_dist(
 def time_to_repair_uniform_dist(
         size: int,
         time_to_repair: datetime.timedelta) -> np.ndarray:
-    """Returns an ndarray of integer hours of the times to repair drawn from
-    a normal distribution with a mean of time_to_repair.
+    """Returns an one-dimensional array of integer hours of the times to repair
+    drawn from a normal distribution with a mean of time_to_repair.
 
-    Draws size samples. The min value is 1.
+    The min value is 1 and the standard deviation is a percentage of the mean.
     """
     mean = total_hours(time_to_repair)
     # Choose a reasonable stddev.
@@ -105,11 +165,7 @@ def time_to_repair_uniform_dist(
     return np.clip(np.rint(normal_dist), a_min=1, a_max=None)
 
 
-time_to_failure_exp_dist(5, mttf=hours(10), sim_duration=hours(50))
-
-
 # %%%
-
 @dataclass(frozen=True, order=True)
 class Outage:
     """An outage is the period of time where a machine is down."""
@@ -120,13 +176,17 @@ class Outage:
     machine: int
 
 
+# TODO: allow the machine outages as a parameter similar to time_to_repair_dist.
 def gen_machine_outages(
         num_machines: int,
         mttf: datetime.timedelta,
-        time_to_repair_dist: RandomDist1dFn,
+        time_to_repair_dist: RandomDistFn,
         sim_duration: datetime.timedelta) -> List[Outage]:
-    """Generates a list of outages from failure starts using a distribution to
-    model the time to repair.
+    """Generates a list of outages for num_machines.
+
+    The outages for each machine are drawn from an exponential distribution using
+    mean time to failure (mttf).  The outage duration is failure start time plus
+    the duration in hours drawn from time_to_repair_dist.
     """
     failure_starts = time_to_failure_exp_dist(
         num_machines, mttf=mttf, sim_duration=sim_duration)
@@ -156,7 +216,15 @@ def gen_machine_outages(
 
 @dataclass(frozen=True, order=True)
 class OutageClique:
-    """Outage cliques occur when multiple machines are down over the same time period."""
+    """An outage clique s when multiple machines are down over the same
+    time period.
+
+    The start_hour for a clique is max of each MachineOutage.start_hour.
+    The end_hour for a clique is the min of each MachineOutage.end_hour.
+    In other words, the duration from start_hour to end_hour is guaranteed
+    to be fully contained in individual machine outage that constitute this
+    clique.
+    """
     # Order is important, we want to sort by hours first.
     start_hour: int
     end_hour: int
@@ -167,8 +235,8 @@ class OutageClique:
         def under(word):
             return '=' * len(word)
 
-        return (f'{"start":<25} {"duration":<20} {"machines"}\n'
-                f'{under("start"):<25} {under("duration"):<20} '
+        return (f'{"outage start":<25} {"duration":<20} {"machines"}\n'
+                f'{under("outage start"):<25} {under("duration"):<20} '
                 f'{under("machines")}')
 
     def format(self):
@@ -180,15 +248,17 @@ class OutageClique:
                 f'{duration:<20} '
                 f'{machines}')
 
+
 # %%
 
 def find_outage_cliques(
         outages: List[Outage],
         clique_size: int) -> List[OutageClique]:
-    """Finds all outage buddies where exactly cliqueSize machines were down
+    """Finds all outage buddies where exactly clique_size machines were down
     in the same time period.
 
-    The clique size will generally be the number of replicas.
+    The clique size will generally be the number of replicas because that
+    potentially indicates that data is completely unavailable.
     """
     if not outages:
         return []
@@ -214,30 +284,32 @@ def find_outage_cliques(
             # Use min because we're not sure which end is smaller.
             clique_end = min(clique_end, next_outage.end_hour)
         else:
-            # The inner loop didn't break, so we have a clique.
-            machines = set(x.machine for x in outages[i:i + clique_size])
+            # The Python for-else statement means the inner loop didn't break,
+            # so we found a clique.
+            machines = frozenset(x.machine for x in outages[i:i + clique_size])
             assert len(machines) == clique_size, 'Found same machine in clique'
             cliques.append(
                 OutageClique(
                     start_hour=clique_start,
                     end_hour=clique_end,
-                    machines=frozenset(sorted(machines))))
+                    machines=machines))
     return cliques
 
 
 # %%
-
 @dataclass(frozen=True)
 class MachinePartition:
-    """A partition is a group of machines.
+    """A partition is a group of machines such that each piece of data is
+    replicated in exactly once partition.
 
-    Each partition contains the same number of machines which should equal the
-    number of replicas.
+    Each partition contains the same number of machines.  Typically, the size
+    of each partition is the num_total_machines / num_partitions.
     """
     machines: FrozenSet[int]
 
 
 def gen_partitions(num_machines: int, num_partitions: int) -> List[MachinePartition]:
+    """Evenly divides num_machines into num_partitions."""
     assert num_machines % num_partitions == 0, (
         'The number of machines must be divisible by the number of partitions.')
     m = num_machines // num_partitions
@@ -250,9 +322,9 @@ def distribute_data(
         num_partitions: int,
         num_replicas: int,
         num_data: int) -> Dict[FrozenSet[int], int]:
-    """Uniformly distributes n pieces of data in partitions.
+    """Uniformly distributes num_data pieces of data in num_partitions.
 
-    - Each piece of data is replicated `replicas` times.
+    - Data is replicated exactly num_replicas times.
     - Data is replicated on machines in the same partition.
     - Data is replicated on different machines within a partition.
     """
@@ -280,7 +352,8 @@ def distribute_data(
     for clique, count in zip(cliques, data_dist):
         counts[clique] = count
 
-    # Add or subtract data so it equals num_data.
+    # Add or subtract data so it equals num_data. We need this because we did
+    # the probabilistic distribution above.
     total = np.sum(data_dist)
     diff = int(num_data - total)
     sign = np.sign(diff)
@@ -291,27 +364,21 @@ def distribute_data(
     return counts
 
 
-a = distribute_data(
-    24,
-    3,
-    3,
-    10000)
-
 # %%
-
 NUM_SIMULATIONS = 10
-SIM_DURATION = datetime.timedelta(days=365 * 4)
+SIM_DURATION = datetime.timedelta(days=365 * 8)
 
-NUM_MACHINES = 100
-NUM_PARTITIONS = 20
-MEAN_TIME_TO_FAILURE = datetime.timedelta(days=30 * 10)
-MEAN_TIME_TO_REPAIR = datetime.timedelta(days=3)
+NUM_MACHINES = 70
+NUM_PARTITIONS = 14
+MEAN_TIME_TO_FAILURE = datetime.timedelta(days=30 * 12 * 2)
+MEAN_TIME_TO_REPAIR = datetime.timedelta(days=2)
 NUM_REPLICAS = 2
 NUM_DATA: int = int(1e6)
-RECOVERY_TIME = datetime.timedelta(days=7)
 
 
 def sim_time_to_repair_dist(size: int) -> np.ndarray:
+    """Curry a time to repair distribution function so it matches the
+    RandomDistFn signature."""
     return time_to_repair_uniform_dist(size, MEAN_TIME_TO_REPAIR)
 
 
@@ -332,10 +399,27 @@ outage_cliques = find_outage_cliques(outages, clique_size=NUM_REPLICAS)
 data_count_by_clique = distribute_data(
     NUM_MACHINES, NUM_PARTITIONS, NUM_REPLICAS, NUM_DATA)
 
-full_outages = [oc for oc in outage_cliques if oc.machines in data_count_by_clique]
-print(f'Full outages: {len(full_outages)}')
+full_outages = [
+    oc for oc in outage_cliques if oc.machines in data_count_by_clique
+]
+
+print(f"""Simulating outages for cell architecture.
+
+SIM_DURATION: {total_months(SIM_DURATION)} months
+NUM_MACHINES: {NUM_MACHINES}
+NUM_PARTITIONS: {NUM_PARTITIONS}
+NUM_REPLICAS: {NUM_REPLICAS}
+NUM_DATA: {NUM_DATA}
+
+MEAN_TIME_TO_FAILURE: {total_months(MEAN_TIME_TO_FAILURE)} months
+MEAN_TIME_TO_REPAIR: {total_days(MEAN_TIME_TO_REPAIR)} days
+""")
+print(f'Number of outages (including partial): {len(outages)}')
+
+print(f'Number of outages with data completely unavailable: {len(full_outages)}')
 print()
 print(OutageClique.table_header())
 print('\n'.join(o.format() for o in full_outages))
 
 # %%
+np.rint(np.sort(np.random.exponential(6, 10)))
